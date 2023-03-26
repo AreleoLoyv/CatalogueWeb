1822e7621118c7480182009c291f589cb460b2b3
AreleoLoyv <airton_loyv@hotmail.com>
2023-03-25 21:37:38 -0500

    catalogue test

---
+++ js/main.js
@@ -0,0 +1,49 @@
+
+function loadApp() {
+
+	// Create the flipbook
+
+	$('.flipbook').turn({
+			// Width
+
+			width:"100vw",
+			
+			// Height
+
+			height:"100vh",
+
+			// Elevation
+
+			elevation: 0,
+			
+			// Enable gradients
+
+			gradients: true,
+			
+			// Auto center this flipbook
+
+			autoCenter: false,
+
+	});
+}
+
+function myFunction() {
+  var flipbook = $("#flipbook");
+  
+  // Obtener el ancho y la altura actuales de la revista
+  var currentWidth = flipbook.width();
+  var currentHeight = flipbook.height();
+  
+  // Actualizar la revista con las nuevas dimensiones
+  flipbook.turn("size", currentWidth, currentHeight);
+};
+
+// Load the HTML4 version if there's not CSS transform
+
+yepnope({
+	test : Modernizr.csstransforms,
+	yep: ['/js/turn.js'],
+	nope: ['/js/turn.html4.min.js'],
+	both: ['css/basic.css'],
+	complete: loadApp
+});
